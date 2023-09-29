6ee05a1631551d5671ade4b81a15595b2423ba30
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-09-29 09:04:00 +0200

    Main module for feature extraction, to be called from worker.callback()

---
+++ feature_extraction.py
@@ -0,0 +1,12 @@
+from torch import nn
+
+def extract_features():
+
+    # consult https://github.com/beeldengeluid/dane-visual-feature-extraction-worker/blob/fae14f7d5ad3e506dbb61315cf8e5b6bb29fe357/feature_examples/feat_demo.py !!
+
+    # Load spectograms + keyframes from file & preprocess
+    # Load model from file
+    # Apply model to data
+    # Binarize resulting feature matrix
+    # Use GPU for processing
+    # Store binarized feature matrix to file
