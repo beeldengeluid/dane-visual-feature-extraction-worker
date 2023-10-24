9d491a9a2fbece5bc8d264e937b00c069c554160
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-10-24 13:27:31 +0200

    Revert "Determine and return corresponding shot boundaries"
    This reverts commit 705225f5f63e6fe89c2b0cb4b316a209545138df.

--- data_handling.py
+++ data_handling.py
@@ -12,37 +12,14 @@ logger = logging.getLogger(__name__)
 
 
 class VisXPData(Dataset):
-    def __init__(self, datapath: Path, model_config_file: str, check_spec_dim=False):
+    def __init__(
+        self, datapath: str | Path, model_config_file: str, check_spec_dim=False
+    ):
         if type(datapath) is not Path:
             datapath = Path(datapath)
-        self.spec_paths = list(datapath.glob("spectograms/*.npz"))
-        self.frame_paths = list(datapath.glob("keyframes/*.jpg"))
-        self.set_config(model_config_file=model_config_file)
-        self.list_of_shots = self.ListOfShots(datapath)
-        if check_spec_dim:
-            all_ok = self.check_spec_dim()
-            if all_ok:
-                logger.info(
-                    "Sampled pectogram dimensionalities match specification"
-                    f" in model config({self.dim_a})"
-                )
+        self.spec_paths = list(datapath.glob("*/spectograms/*.npz"))
+        self.frame_paths = list(datapath.glob("*/keyframes/*.jpg"))
 
-    def check_spec_dim(self, sample_size=5):
-        all_ok = True
-        for index in random.sample(range(len(self.spec_paths)), sample_size):
-            spec = self.__get_spec__(index=index, transform=False)
-            try:
-                assert spec.shape == self.dim_a
-            except AssertionError:
-                logger.info(
-                    f"Spectogram dimensionality ({spec.shape})"
-                    f" does not match specification in "
-                    f"model config({self.cfg.SPECTOGRAM.DIMENSIONALITY})"
-                )
-                all_ok = False
-        return all_ok
-
-    def set_config(self, model_config_file: str):
         with open(model_config_file, "r") as f:
             cfg = CN.load_cfg(f).INPUT
             norm_a = eval(cfg.SPECTOGRAM.NORMALIZATION)
@@ -61,17 +38,43 @@ class VisXPData(Dataset):
                 ]
             )
 
+        if check_spec_dim:
+            all_ok = True
+            for index in random.sample(range(len(self.spec_paths)), 5):
+                ok = self.check_spec_dim(index)
+                all_ok = min(ok, all_ok)
+            if all_ok:
+                logger.info(
+                    "Spectogram dimensionalities match specification in "
+                    f"model config({self.dim_a})"
+                )
+
+    def check_spec_dim(self, index):
+        spec = self.__get_spec__(index=index, transform=False)
+        try:
+            assert spec.shape == self.dim_a
+        except AssertionError:
+            logger.info(
+                f"Spectogram dimensionality ({spec.shape})"
+                f" does not match specification in "
+                f"model config({self.cfg.SPECTOGRAM.DIMENSIONALITY})"
+            )
+            return False
+        return True
+
     def __len__(self):
         return len(self.spec_paths)
 
     def __getitem__(self, index):
+        frame = self.__get_keyframe__(index=index)
+        audio = self.__get_spec__(index=index)
+
         item_dict = dict()
-        item_dict["video"] = self.__get_keyframe__(index=index)
-        item_dict["audio"] = self.__get_spec__(index=index)
-        timestamp = int(self.frame_paths[index].parts[-1].split(".")[0])
-        item_dict["timestamp"] = timestamp
-        item_dict['shot_boundaries'] = self.list_of_shots.find_shot_for_timestamp(
-            timestamp=timestamp)
+        item_dict["video"] = frame
+        item_dict["audio"] = audio
+        item_dict["videoname"] = self.frame_paths[index].parts[0]
+        item_dict["timestamp"] = self.frame_paths[index].parts[-1].split(".")[0]
+
         return item_dict
 
     def __get_spec__(self, index, transform=True):
@@ -87,21 +90,5 @@ class VisXPData(Dataset):
         frame = self.visual_transform(frame)
         return frame
 
-    def batches(self, batch_size: int = 1):
-        return DataLoader(self, batch_size=batch_size)
-
-    class ListOfShots:
-        def __init__(self, datapath: Path):
-            with open(datapath / 'metadata' / 'shot_boundaries_timestamps_ms.txt',
-                      'r') as f:
-                self.list_of_tuples = np.array(eval(f.read()))
-
-        def find_shot_for_timestamp(self, timestamp):
-            a = self.list_of_tuples
-            hits = a[np.logical_and(a[:, 0] <= timestamp, a[:, 1] >= timestamp)]
-            if len(hits != 0):
-                # matched one (or multiple) shots: return boundaries (of the first hit)
-                return hits[0]
-            else:
-                # No corresponding shot (shots may be un-covering). Signal with (-1,-1)
-                return np.array((-1, -1))
+    def batches(self):
+        return DataLoader(self)
