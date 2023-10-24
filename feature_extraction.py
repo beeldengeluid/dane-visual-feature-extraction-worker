e41adc24eff3ee7a3d9483787b9f1df2932877a5
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-10-24 13:27:28 +0200

    Revert "Clearer division of labor, generate provenance"
    This reverts commit 85f91f178cbfe447cffea0fcf40a1cef66b99810.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,32 +1,22 @@
 import logging
-from time import time
-
 from nn_models import load_model_from_file
 import sys
 import torch
+import numpy as np
 import os
 from pathlib import Path
 from data_handling import VisXPData
-from models import VisXPFeatureExtractionOutput
-from provenance import generate_full_provenance_chain
-from output_util import get_source_id, export_features
+
 
 logger = logging.getLogger(__name__)
 
 
-def extract_features(
-        input_path: str,
-        model_path: str,
-        model_config_file: str,
-        output_path: str) -> VisXPFeatureExtractionOutput:
-    start_time = time()
-    # Step 0: this is the "processing ID" if you will
-    source_id = get_source_id(input_path)
-    logger.info(f"Extracting features for: {source_id}.")
-
+def extract_features(input_path: str, source_id, model_path, model_config_file):
+    # consult misc/feature_examples/feat_demo.py !!
+    source_path = Path(input_path) / source_id
     # Load spectograms + keyframes from file & preprocess
     dataset = VisXPData(
-        Path(input_path), model_config_file=model_config_file
+        source_path, model_config_file=model_config_file
     )
 
     # Load model from file
@@ -38,7 +28,7 @@ def extract_features(
     # Apply model to data
     logger.info(f"Going to extract features for {dataset.__len__()} items. ")
 
-    result = torch.Tensor([])
+    result = None
     for i, batch in enumerate(dataset.batches(batch_size=256)):
         frames, spectograms = batch["video"], batch["audio"]
         timestamps, shots = batch["timestamp"], batch["shot_boundaries"]
@@ -48,21 +38,24 @@ def extract_features(
         batch_result = torch.concat(
             (timestamps.unsqueeze(1), shots, audio_feat, visual_feat),
             1)
-        result = torch.concat((result, batch_result), 0)
-    destination = os.path.join(output_path, f'{source_id}.pt')
-    export_features(result, destination=destination)
-    provenance = generate_full_provenance_chain(
-        start_time=start_time,
-        input_path=input_path,
-        provenance_chain=[],
-        output_path=destination
-    )
-    return VisXPFeatureExtractionOutput(
-        200, "Succesfully extracted features", provenance)
+        if not result:
+            result = batch_result
+        else:
+            result = torch.concat((result, batch_result), 0)
+    export_features(result,
+                    destination=os.path.join('data/visxp_features', f'{source_id}.pt'))
 
     # Binarize resulting feature matrix
     # Use GPU for processing
     # Store binarized feature matrix to file
+
+
+def export_features(features: torch.Tensor, destination: str):
+    with open(os.path.join(destination), 'wb') as f:
+        torch.save(obj=features, f=f)
+
+
+
 
 
 if __name__ == "__main__":
@@ -73,8 +66,11 @@ if __name__ == "__main__":
         "|%(funcName)s|%(lineno)d|%(message)s",
     )
 
-    extract_features(input_path="data/visxp_prep/ZQWO_DYnq5Q_000000",
-                     output_path="data/visxp_features",
+    data_path = "data/visxp_prep"
+    # data_path = "data/proper_data"
+
+    extract_features(input_path=data_path,
+                     source_id='ZQWO_DYnq5Q_000000',
                      model_config_file='models/model_config.yml',
                      model_path='models/checkpoint.tar')
 
