27da7979060e45be68a616ee318d86cf72428ab3
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-10-30 15:09:36 +0100

    Assert features correspond to example features

--- tests/unit/feature_extraction_test.py
+++ tests/unit/feature_extraction_test.py
@@ -1,14 +1,9 @@
 import feature_extraction
 import os
 import torch
-import numpy as np
-import logging
+
 
 def test_extract_features():
-    
-    #assert True
-    #return
-    
     feature_extraction.extract_features(
         input_path='tests/data',
         model_path='models/checkpoint.tar',
@@ -17,13 +12,14 @@ def test_extract_features():
     feature_file = 'tests/data/data.pt'
     with open(feature_file, 'rb') as f:
         features = torch.load(f)
+    with open('tests/data/demo_concat_feat.pt', 'rb') as f:
+        example_features = torch.load(f)
+
+    # make sure that we're comparing the proper vectors
+    assert torch.equal(features[:, 0], example_features[:, 0])
+
     features = features[:, 3:]  # columns 0,1,2 hold timestamps & shot boundaries
-    example_features = torch.Tensor(np.load('tests/data/demo_concat_feat.npy'))
-    
-    
-
-
+    example_features = example_features[:, 1:]  # column 0 holds timestamps/indices
     assert torch.equal(features, example_features)
 
     os.remove(feature_file)
-    assert feature_extraction.example_function()
