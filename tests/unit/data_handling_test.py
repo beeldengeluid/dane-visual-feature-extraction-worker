b774c1915c9adf6424feacc85dfb096826d5d493
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-05-13 16:43:42 +0200

    run datahandling tests for both visual-only and (legacy) AV processing

--- tests/unit/data_handling_test.py
+++ tests/unit/data_handling_test.py
@@ -4,23 +4,29 @@ import torch
 
 def test_batches():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    dataset = VisXPData(
-        datapath="data/input-files/test_source_id",
-        model_config_file="model/model_config.yml",
-        device=device,
-        check_spec_dim=False,
-    )
-    for i, item in enumerate(dataset.batches(1)):
-        index = int(item["timestamp"][0])
+    for audio_too in [True, False]:
+        dataset = VisXPData(
+            datapath="data/input-files/test_source_id",
+            model_config_file=(
+                "model/model_config.yml" if audio_too else
+                "model/visualnet_config.yml"
+            ),
+            device=device,
+            audio_too=audio_too
+        )
+        for i, item in enumerate(dataset.batches(1)):
+            index = int(item["timestamp"][0])
 
-        for kind in ["video", "audio"]:
-            this = item[kind][0]
-        example = obtain_example(index, kind)
-        assert torch.equal(example, this)
+            for kind in ["video", "audio"]:
+                if not audio_too and kind == "audio":
+                    continue
+                this = item[kind][0]
+                example = obtain_example(index, kind)
+                assert torch.equal(example, this)
 
 
 def obtain_example(i, kind):
-    assert kind in ["frame", "audio"]
+    assert kind in ["video", "audio"]
     example_path = f"data/example_dataset/demo_{kind}_{i}.pt"
     with open(example_path, "rb") as f:
         example = torch.load(f)
