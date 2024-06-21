b1861cdde175aaa83762bbf288e9081bc13893c0
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-06-21 16:28:33 +0200

    Parametrize test_batches

--- tests/unit/data_handling_test.py
+++ tests/unit/data_handling_test.py
@@ -1,27 +1,28 @@
 from data_handling import VisXPData
 import torch
+import pytest
 
 
-def test_batches():
+@pytest.mark.parametrize("audio_too", [True, False])
+def test_batches(audio_too):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    for audio_too in [True, False]:
-        dataset = VisXPData(
-            datapath="data/input-files/test_source_id",
-            model_config_file=(
-                "model/model_config.yml" if audio_too else "model/visualnet_config.yml"
-            ),
-            device=device,
-            audio_too=audio_too,
-        )
-        for i, item in enumerate(dataset.batches(1)):
-            index = int(item["timestamp"][0])
+    dataset = VisXPData(
+        datapath="data/input-files/test_source_id",
+        model_config_file=(
+            "model/model_config.yml" if audio_too else "model/visualnet_config.yml"
+        ),
+        device=device,
+        audio_too=audio_too,
+    )
+    for i, item in enumerate(dataset.batches(1)):
+        index = int(item["timestamp"][0])
 
-            for kind in ["video", "audio"]:
-                if not audio_too and kind == "audio":
-                    continue
-                this = item[kind][0]
-                example = obtain_example(index, kind)
-                assert torch.equal(example, this)
+        for kind in ["video", "audio"]:
+            if not audio_too and kind == "audio":
+                continue
+            this = item[kind][0]
+            example = obtain_example(index, kind)
+            assert torch.equal(example, this)
 
 
 def obtain_example(i, kind):
